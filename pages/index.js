b876f89c02b98de8817f265154db438df4461dab
Zgîmbău Tudor <tudor.zgimbau@gmail.com>
2022-03-10 18:26:59 +0200

    Update credits

--- pages/index.js
+++ pages/index.js
@@ -85,7 +85,7 @@ export default function Home() {
         }} justifyItems="center">
           <Text fontSize="14px" marginRight="10px">developed in</Text>
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 3 2"><path fill="#002B7F" d="M0 0h3v2H0z" /><path fill="#FCD116" d="M1 0h2v2H1z" /><path fill="#CE1126" d="M2 0h1v2H2z" /></svg>
-          <Text marginLeft="10px" fontSize="14px">by <Link href="https://github.com/TudorZgimbau" color="orange.200" isExternal>Tudor Zgîmbău</Link> and <Link href="https://www.linkedin.com/in/mihai-cosmin-ivanov-4136a9195" color="orange.200" isExternal>Mihai Cosmin Ivanov</Link></Text>
+          <Text marginLeft="10px" fontSize="14px">by <Link href="https://www.linkedin.com/in/tudor-zg%C3%AEmb%C4%83u-a85274234/" color="orange.200" isExternal>Tudor Zgîmbău</Link>
         </Flex>
       </Flex>
     </>
