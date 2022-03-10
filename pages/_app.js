5ed0a8fd4e33dbbbc31ee2997af40f080c650455
TudorZgimbau <tudor.zgimbau@gmail.com>
2022-03-10 11:20:52 +0200

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { DarkMode, GlobalStyle } from '@chakra-ui/react'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(async () => {
+    let page = document.URL.substring(39);
+    console.log(page);
+    if (page === "")
+      page = "index";
+    await fetch(process.env.NEXT_PUBLIC_ANALYTICS_SERVER + "/analytics/" + page);
+  })
+  
   return (
     <ChakraProvider>
       <DarkMode>
