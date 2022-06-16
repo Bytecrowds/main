7c146366ddf3f5dea2291111ae82f29abdcae9f7
TudorZgimbau <tudor.zgimbau@gmail.com>
2022-06-16 20:49:26 +0300

    updated and formatted the code to use the latest versions of the dependencies

--- realtime/store.js
+++ realtime/store.js
@@ -2,22 +2,21 @@ import { syncedStore, getYjsValue } from "@syncedstore/core";
 import { WebsocketProvider } from "y-websocket";
 import userColor from "../utils/color";
 
-
 const store = syncedStore({ bytecrowdText: "text" });
 export default store;
 
 const doc = getYjsValue(store);
 
 export const getWebSocketProvider = (id) => {
-    const webSocketProvider = new WebsocketProvider(
-        process.env.NEXT_PUBLIC_WEBSOCKET_SERVER,
-        id,
-        doc
-    );
-    webSocketProvider.awareness.setLocalStateField('user', {
-        name: 'Anonymous ' + Math.floor(Math.random() * 100),
-        color: userColor.color,
-        colorLight: userColor.light
-    })
-    return webSocketProvider;
-}
+  const webSocketProvider = new WebsocketProvider(
+    process.env.NEXT_PUBLIC_WEBSOCKET_SERVER,
+    id,
+    doc
+  );
+  webSocketProvider.awareness.setLocalStateField("user", {
+    name: "Anonymous " + Math.floor(Math.random() * 100),
+    color: userColor.color,
+    colorLight: userColor.light,
+  });
+  return webSocketProvider;
+};
