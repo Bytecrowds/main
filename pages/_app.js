51dfa30738eac36fdfb69a1e3fa805fd77b3d9f7
Zgîmbău Tudor <tudor.zgimbau@gmail.com>
2023-07-16 11:46:55 +0300

    Add LogRocket

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,13 @@ import { SessionProvider } from "next-auth/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { useEffect } from "react";
 import theme from "../theme";
+import LogRocket from "logrocket";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  // Send the IP adress and current page to the analytics server.
   useEffect(() => {
+    LogRocket.init("ptmrhf/bytecrowds");
+
+    // Send the IP adress and current page to the analytics server.
     async function fetchAnalytics() {
       let page;
       let environment = process.env.NEXT_PUBLIC_ENVIRONMENT;
